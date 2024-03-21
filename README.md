d19b1aef5d7f27d60628b82901badb755c659024
Aiman khan <aimhanif321@gmail.com>
2024-03-21 17:29:59 +0500

    push my calculator on github

---
+++ README.md
@@ -0,0 +1,1 @@
+"# pro-simple-calculator" 
---
+++ index.js
@@ -0,0 +1,23 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+  { message: "enter your first number", type: "number", name: "firstnumber" },
+  { message: "enter second number", type: "number", name: "secondnumber" },
+  {
+    message: "select one of the oprator",
+    type: "list",
+    name: "oprator",
+    choices: ["Addition", "subtraction", "multiplication", "division"],
+  },
+]);
+if (answer.oprator === "Addition") {
+  console.log("your value is" + answer.firstnumber + answer.secondnumber);
+} else if (answer.oprator === "subtraction") {
+  console.log(answer.firstnumber - answer.secondnumber);
+} else if (answer.oprator === "multiplication") {
+  console.log(answer.firstnumber * answer.secondnumber);
+} else if (answer.oprator === "division") {
+  console.log(answer.firstnumber / answer.secondnumber);
+} else {
+  console.log("your number is invalid");
+}
